f42efb5099a7777604c3a9ecb399553ae430465e
Evan Hughes <24ehughes@student.dasd.org>
2024-01-26 09:04:53 -0500

    update to word systems

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='GmailDelete',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='24ehughes',
+    author_email='',
+    description=''
+)
